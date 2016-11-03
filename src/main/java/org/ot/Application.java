d05ec2dc67e83a34f03af1b6c7fb22131e10177c
Maciej Miklas <mac.miklas@gmail.com>
2016-11-03 08:51:44 +0100

    first reelase

---
+++ src/main/java/org/ot/Application.java
@@ -0,0 +1,14 @@
+package org.ot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+    static ApplicationContext ctx;
+
+    public static void main(String[] args) {
+	ctx = SpringApplication.run(Application.class, args);
+    }
+}
